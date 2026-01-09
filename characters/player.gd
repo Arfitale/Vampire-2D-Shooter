98c3ea9206f13503c2ad51908e9eca5760f952b8
arfitale <arfitale@gmail.com>
2026-01-09 08:22:45 +0700

    Enhanced player movement script to ensure proper animation playback based on velocity.

--- characters/player.gd
+++ characters/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 @export var movement_speed := 600.0
+@onready var sprite := $HappyBoo
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * movement_speed
+	
+	if velocity.length() > 0.0:
+		sprite.play_walk_animation()
+	else:
+		sprite.play_idle_animation()
+	
 	move_and_slide()
